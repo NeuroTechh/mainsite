014f79a9015a5b241d90a20aefa28e004f44e8fe
Yash <78804783+yashd-dev@users.noreply.github.com>
2024-04-11 18:12:35 +0530

    SEO, Analytics and UI Tweaks

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,17 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
+import vercel from "@astrojs/vercel/serverless";
 
+import sitemap from "@astrojs/sitemap";
+
+// https://astro.build/config
 export default defineConfig({
-	integrations: [tailwind()],
+  site: "https://neurotechh.live/",
+  integrations: [tailwind(), sitemap()],
+  output: "server",
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true
+    }
+  })
 });
