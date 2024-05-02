625b8112a960a908b8a885ef834bece30fd9e9ab
Yash <78804783+yashd-dev@users.noreply.github.com>
2024-05-03 02:15:42 +0530

    Added Vercel Adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,18 +2,17 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import sanity from "@sanity/astro";
 import node from "@astrojs/node";
-
 import sitemap from "@astrojs/sitemap";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://neurotechh.live/",
-	integrations: [tailwind(), sitemap()],
-	output: "server",
-	adapter: node({
-		mode: "standalone",
-	}),
-	prefetch: {
-		include: "all",
-	},
+  site: "https://neurotechh.live/",
+  integrations: [tailwind(), sitemap()],
+  output: "server",
+  adapter: vercel(),
+  prefetch: {
+    include: "all"
+  }
 });
