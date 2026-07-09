60e67b71961b97470d2aad073669b917423c5bc1
Kartik Jain <info@jkartik.in>
2026-07-09 21:17:42 +0530

    Upgrade Astro 7 and Tailwind 4

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,23 +1,30 @@
 import { defineConfig } from "astro/config";
+
 import react from "@astrojs/react";
 import sitemap from "@astrojs/sitemap";
 import node from "@astrojs/node";
+import sanity from "@sanity/astro";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  site: "https://neurotechh.mpst.me",
+  site: "https://neurotechh.xyz",
+
   output: "server",
+
   adapter: node({
     mode: "standalone",
   }),
+
   integrations: [
     react(),
     sitemap(),
     sanity(),
   ],
+
   vite: {
     plugins: [tailwindcss()],
   },
+
   prefetch: {
     prefetchAll: true,
   },
